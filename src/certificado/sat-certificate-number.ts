f57cd9a2932139d4cdb18c87b8a4e149fc7f1f8b
Fernando Isidro L <luffynando@gmail.com>
2022-02-24 17:58:25 -0600

    feat: migrate from php to node SatCertificateNumber

---
+++ src/certificado/sat-certificate-number.ts
@@ -0,0 +1,31 @@
+export class SatCertificateNumber {
+    private readonly _id: string;
+
+    constructor(id: string) {
+        if (!SatCertificateNumber.isValidCertificateNumber(id)) {
+            throw new SyntaxError('The certificate number is not correct');
+        }
+        this._id = id;
+    }
+
+    public number(): string {
+        return this._id;
+    }
+
+    public remoteUrl(): string {
+        return [
+            'https://rdc.sat.gob.mx/rccf',
+            `/${this._id.substring(0, 6)}`,
+            `/${this._id.substring(6, 12)}`,
+            `/${this._id.substring(12, 14)}`,
+            `/${this._id.substring(14, 16)}`,
+            `/${this._id.substring(16, 18)}`,
+            `/${this._id}`,
+            '.cer',
+        ].join('');
+    }
+
+    public static isValidCertificateNumber(id: string): boolean {
+        return /^[0-9]{20}$/.test(id);
+    }
+}
---
+++ tests/unit/sat-certificate-number.test.ts
@@ -0,0 +1,30 @@
+import { SatCertificateNumber } from '../../src/certificado/sat-certificate-number';
+
+describe('SatCertificateNumber', () => {
+    test.each([['00000000000000000000'], ['98765432109876543210']])(
+        'is valid certificate number with correct values %s',
+        (value: string) => {
+            expect(SatCertificateNumber.isValidCertificateNumber(value)).toBeTruthy();
+
+            const number = new SatCertificateNumber(value);
+            expect(number.number()).toBe(value);
+            expect(number.remoteUrl().endsWith(`${value}.cer`)).toBeTruthy();
+        }
+    );
+
+    test.each([
+        ['empty', ''],
+        ['with-non-digits', 'A0000000000000000000'],
+        ['length 19', '0000000000000000000'],
+        ['length 21', '000000000000000000000'],
+    ])('is valid certificate number with incorrect values %s', (name: string, value: string) => {
+        expect(SatCertificateNumber.isValidCertificateNumber(value)).toBeFalsy();
+        expect.hasAssertions();
+        try {
+            new SatCertificateNumber(value);
+        } catch (e) {
+            expect(e).toBeInstanceOf(SyntaxError);
+            expect(e).toHaveProperty('message', 'The certificate number is not correct');
+        }
+    });
+});
