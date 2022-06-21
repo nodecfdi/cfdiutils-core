a4cbd3775876385da80765756f577d9113e6fb08
Fernando Isidro L <luffynando@gmail.com>
2022-06-21 12:54:50 -0500

    fix: stdout being truncated

--- src/cadena-origen/saxonb-cli-builder.ts
+++ src/cadena-origen/saxonb-cli-builder.ts
@@ -2,7 +2,7 @@ import { AbstractXsltBuilder } from './abstract-xslt-builder';
 import { accessSync, constants, existsSync, statSync, write } from 'fs';
 import { XsltBuildException } from './xslt-build-exception';
 import { cleanupSync, open } from 'temp';
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 
 export class SaxonbCliBuilder extends AbstractXsltBuilder {
     private _executablePath!: string;
@@ -52,24 +52,24 @@ export class SaxonbCliBuilder extends AbstractXsltBuilder {
                     args.push(`-s:${info.path}`);
                     args.push(`-xsl:${xsltLocation}`);
                     args.push('-warnings:silent');
-                    const xslt = spawn(executable, args);
-                    xslt.stdout.on('data', (data: string) => {
-                        cleanupSync();
-                        if ('<?xml version="1.0" encoding="UTF-8"?>' === `${data}`.trim()) {
-                            reject(new XsltBuildException('Transformation error'));
+                    const finalCommand = `${executable} ${args.join(' ')}`;
+                    exec(
+                        finalCommand,
+                        {
+                            maxBuffer: 1024 * 1024 * 100, // 100MB
+                        },
+                        (error, stdout, stderr) => {
+                            cleanupSync();
+                            if (error) {
+                                return reject(new XsltBuildException(`Transformation error: ${stderr}`));
+                            }
+
+                            if ('<?xml version="1.0" encoding="UTF-8"?>' === `${stdout}`.trim()) {
+                                return reject(new XsltBuildException('Transformation error'));
+                            }
+                            return resolve(`${stdout}`.trim());
                         }
-                        return resolve(`${data}`.trim());
-                    });
-
-                    xslt.stderr.on('data', (data: string) => {
-                        cleanupSync();
-                        return reject(new XsltBuildException(`Transformation error: ${data}`));
-                    });
-
-                    xslt.on('exit', (code: number | string) => {
-                        cleanupSync();
-                        return reject(new XsltBuildException(`Transformation error with code: ${code}`));
-                    });
+                    );
                 });
             });
         });
